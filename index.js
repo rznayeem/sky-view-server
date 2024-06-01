e9c8fa875bc9dba68661acb4526db4476c2e2777
rznayeem <riyazulnayeem@gmail.com>
2024-06-02 05:12:16 +0600

    apartment api created

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const apartmentCollection = client.db('skyViewDB').collection('apartments');
+
+    app.get('/apartment', async (req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
