1c36820181cc3b2515c8614684bb66974b717985
rznayeem <riyazulnayeem@gmail.com>
2024-06-10 03:02:52 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const apartmentCollection = client.db('skyViewDB').collection('apartments');
     const userCollection = client.db('skyViewDB').collection('users');
@@ -297,7 +297,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!'
     );
